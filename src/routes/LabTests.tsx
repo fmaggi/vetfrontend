920be6e3a8ab72880d102c10f95b1a8ccf61692c
Francisco <franciscomaggi1999@gmail.com>
2023-03-07 11:20:31 -0300

    fixed sidebar and content size

--- src/routes/LabTests.tsx
+++ src/routes/LabTests.tsx
@@ -5,68 +5,70 @@ import TestForm from '../components/TestForm';
 import tests from '../data/tests';
 
 import {
-  Heading,
-  Center,
-  Tabs,
-  TabList,
-  Tab,
-  TabPanels,
-  TabPanel,
-  Box,
-  useColorModeValue
+    Heading,
+    Center,
+    Tabs,
+    TabList,
+    Tab,
+    TabPanels,
+    TabPanel,
+    Box,
+    useColorModeValue
 } from '@chakra-ui/react'
 import saveTestToPDF from '../utils/saveTestToPdf';
+import { useWindowSize } from '../hooks/window';
 
 interface ActionLoaderProps {
-  request?: any,
-  params?: any
+    request?: any,
+    params?: any
 }
 
 export async function loader({ params }: ActionLoaderProps) {
-  const patient = params.patient;
-  return patient;
+    const patient = params.patient;
+    return patient;
 }
 
 export async function action({ request }: ActionLoaderProps) {
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
-  const { testName, patient, ...results } = data;
-  const errs = saveTestToPDF(testName, patient, results, 'test.pdf');
-  if (errs) {
-    alert('Error creando el pdf');
-    console.log(errs);
-  }
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+    const { testName, patient, ...results } = data;
+    const errs = saveTestToPDF(testName, patient, results, 'test.pdf');
+    if (errs) {
+        alert('Error creando el pdf');
+        console.log(errs);
+    }
 
-  return null;
+    return null;
 }
 
 export default function LabTests() {
-  const patient = useLoaderData() as string;
-  return (
-    <Box w='100%' h='100%' shadow='none' bg={useColorModeValue('white', 'gray.700')} px={5}>
-      <Box m={2}>
-        <Heading as='h6'>{patient}</Heading>
-      </Box>
-      <Tabs>
-        <TabList>
-          {
-            tests.map(test => (<Tab key={test.name}>{test.name}</Tab>))
-          }
-        </TabList>
-        <TabPanels>
-          {
-            tests.map(test => {
-              return (
-                <TabPanel key={test.name}>
-                  <Center>
-                    <TestForm test={test} patient={patient} width='70%' />
-                  </Center>
-                </TabPanel>
-              )
-            })
-          }
-        </TabPanels>
-      </Tabs>
-    </Box>
-  )
+    const patient = useLoaderData() as string;
+    const winSize = useWindowSize();
+    return (
+        <Box w='100%' h={winSize.h} shadow='none' bg={useColorModeValue('white', 'gray.700')} px={5}>
+            <Box m={2}>
+                <Heading as='h6'>{patient}</Heading>
+            </Box>
+            <Tabs>
+                <TabList>
+                    {
+                        tests.map(test => (<Tab key={test.name}>{test.name}</Tab>))
+                    }
+                </TabList>
+                <TabPanels>
+                    {
+                        tests.map(test => {
+                            return (
+                                <TabPanel key={test.name}>
+                                    <Center>
+                                        <TestForm test={test} patient={patient} width='70%' />
+                                    </Center>
+                                </TabPanel>
+                            )
+                        })
+                    }
+                </TabPanels>
+            </Tabs>
+        </Box>
+    )
 }
