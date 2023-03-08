d033d2f397de24b71e04929b8f4cf8853515de97
Francisco <franciscomaggi1999@gmail.com>
2023-03-08 10:53:43 -0300

    changes to stats page

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import Root from './routes/Root';
 import LabTests, { action as savePdf, loader as loadPatient } from './routes/LabTests';
-import Patients, { loader as loadPatients} from './routes/Patients';
-import Home, {} from './routes/Home';
-import Stats, {loader as loadStats} from './routes/Stats';
+import Patients, { loader as loadPatients } from './routes/Patients';
+import Home, { } from './routes/Home';
+import Stats, { loader as loadStats } from './routes/Stats';
 import Error from './routes/Error';
 
 import { createHashRouter, RouterProvider } from "react-router-dom";
@@ -12,42 +12,42 @@ import { createHashRouter, RouterProvider } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react'
 
 const router = createHashRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: 'labs/:patient',
-        element: <LabTests />,
-        loader: loadPatient,
-        action: savePdf
-      },
-      {
-        path: 'pacientes',
-        element: <Patients />,
-        loader: loadPatients
-      },
-      {
-        path: 'estadisticas/:days?',
-        element: <Stats />,
-        loader: ({params}) => loadStats(Number(params.days)),
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: 'labs/:patient',
+                element: <LabTests />,
+                loader: loadPatient,
+                action: savePdf
+            },
+            {
+                path: 'pacientes',
+                element: <Patients />,
+                loader: loadPatients
+            },
+            {
+                path: 'estadisticas/:days?',
+                element: <Stats />,
+                loader: loadStats,
 
-      }
-    ]
-  },
+            }
+        ]
+    },
 ]);
 
 function App() {
-  return (
-    <ChakraProvider>
-      <RouterProvider router={router} />
-    </ChakraProvider>
-  );
+    return (
+        <ChakraProvider>
+            <RouterProvider router={router} />
+        </ChakraProvider>
+    );
 }
 
 export default App;
--- src/routes/Stats.tsx
+++ src/routes/Stats.tsx
@@ -1,4 +1,4 @@
-import {useFetcher} from 'react-router-dom'
+import { useFetcher } from 'react-router-dom'
 
 
 
@@ -13,22 +13,34 @@ import {
 } from '@chakra-ui/react'
 
 import tests from "../data/tests";
-import {ChevronDownIcon} from "@chakra-ui/icons";
-import {useState} from 'react';
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { ReactNode, useEffect, useState } from 'react';
 
-export /*async*/ function loader(days: number) {
+export async function loader({ params }: any) {
     /*
     fetch(BACKEND).then(res=>res.json());
 
      */
+    var days = Number(params.days);
     if (Number.isNaN(days) || days === -1)
         days = 100;
-    return tests[0].fields.map(field => {
+    const stats = tests[0].fields.map(field => {
         return {
             name: field.label,
             value: Math.floor(Math.random() * days) + 1
         }
     });
+
+    return { stats: stats, days: days }
+}
+
+
+function FormButton({ fetcher, action, children }: { fetcher: any, action: string, children: ReactNode }) {
+    return (
+        <fetcher.Form method='get' action={action}>
+            <MenuItem type='submit'>{children}</MenuItem>
+        </fetcher.Form>
+    );
 }
 
 
@@ -36,26 +48,16 @@ export default function Stats() {
 
     const bg = useColorModeValue('white', 'gray.700')
     const fetcher = useFetcher();
-    const [menuText, setMenuText] = useState("Periodo");
-    const [dias, setDias] = useState(100);
 
+    useEffect(() => {
+        if (!fetcher.data && fetcher.state === 'idle') {
+            fetcher.submit({ method: 'get', action: '/estadisticas' });
+        }
+    }, [fetcher]);
 
-    function menuAction(days : number){
-        if (days === -1)
-            setMenuText("Todo");
-        else
-            setMenuText(`Ultimos ${days} dias`)
-        setDias(days);
-    }
-
-
-    if(!fetcher.data && fetcher.state === "idle"){
-        fetcher.load(`/estadisticas`);
-    }
     return (
 
         <SimpleGrid w='100%' h='100vh' p='5' columns={1} spacing={5}>
-
             <Card key={"period_chooser"}>
                 <CardHeader>
                     <Heading>
@@ -65,43 +67,45 @@ export default function Stats() {
                 <CardBody>
                     <Menu>
                         <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                            {menuText}
+                            {
+                                fetcher.data ?
+                                    fetcher.data.days === 100 ? 'Todo' : `Ultimos ${fetcher.data.days} dias`
+                                    : 'Periodo'
+                            }
+
                         </MenuButton>
-                        <fetcher.Form method="get" action={`/estadisticas/${dias}`}>
-                            <MenuList>
-                                <MenuItem type="submit" onClick={() => menuAction(7)}>Ultimos 7 dias</MenuItem>
-                                <MenuItem type="submit" onClick={() => menuAction(15)}>Ultimos 15 dias</MenuItem>
-                                <MenuItem type="submit" onClick={() => menuAction(30)}>Ultimos 30 dias</MenuItem>
-                                <MenuItem type="submit" onClick={() => menuAction(90)}>Ultimos 90 dias</MenuItem>
-                                <MenuItem type="submit" onClick={() => menuAction(-1)}>Todo</MenuItem>
-                            </MenuList>
-                        </fetcher.Form>
+                        <MenuList>
+                            <FormButton action={'/estadisticas/7'} fetcher={fetcher}>Ultimos 7 dias</FormButton>
+                            <FormButton action={'/estadisticas/15'} fetcher={fetcher}>Ultimos 15 dias</FormButton>
+                            <FormButton action={'/estadisticas/30'} fetcher={fetcher}>Ultimos 30 dias</FormButton>
+                            <FormButton action={'/estadisticas/90'} fetcher={fetcher}>Ultimos 90 dias</FormButton>
+                            <FormButton action={'/estadisticas'} fetcher={fetcher}>Todo</FormButton>
+                        </MenuList>
                     </Menu>
                 </CardBody>
             </Card>
             {
                 fetcher.data ?
-                    fetcher.data.map((test: any) =>
-                        {
-                            return (
-                                <Card key={test.name} bg={bg} shadow='sm'>
-                                    <CardHeader>
-                                        <Heading>{test.name}</Heading>
-                                    </CardHeader>
-                                    <CardBody>
-                                        <Flex align='center' justify='space-between'>
-                                            <Text>
-                                                Cantidad: {test.value}
-                                            </Text>
-                                        </Flex>
-                                    </CardBody>
-                                </Card>
-                            )
-                        }
+                    fetcher.data.stats.map((test: any) => {
+                        return (
+                            <Card key={test.name} bg={bg} shadow='sm'>
+                                <CardHeader>
+                                    <Heading>{test.name}</Heading>
+                                </CardHeader>
+                                <CardBody>
+                                    <Flex align='center' justify='space-between'>
+                                        <Text>
+                                            Cantidad: {test.value}
+                                        </Text>
+                                    </Flex>
+                                </CardBody>
+                            </Card>
+                        )
+                    }
                     )
                     : <Text> Loading... </Text>
             }
 
-        </SimpleGrid>
+        </SimpleGrid >
     )
 }
