e717e264524e0967a0fa878764ef76de34664265
Francisco <franciscomaggi1999@gmail.com>
2023-03-08 11:22:38 -0300

    added observations

--- src/components/TestForm.tsx
+++ src/components/TestForm.tsx
@@ -11,7 +11,8 @@ import {
     InputGroup,
     InputRightAddon,
     Center,
-    Box
+    Box,
+    Textarea
 } from '@chakra-ui/react'
 
 export interface TestFormProps {
@@ -25,34 +26,39 @@ export default function TestForm({ test, patient, width }: TestFormProps) {
 
     return (
         <Box w={w}>
-        <Form method='post'>
-            <VStack spacing={5}>
-                <Input type='text' defaultValue={test.name} name='testName' hidden/>
-                <Input type='text' defaultValue={patient} name='patient' hidden/>
-            {
-                test.fields.map(entry => {
-                    const label = entry.label;
-                    const name = Test.LabelToName(label);
+            <Form method='post'>
+                <VStack spacing={5}>
+                    <Input type='text' defaultValue={test.name} name='testName' hidden />
+                    <Input type='text' defaultValue={patient} name='patient' hidden />
+                    {
+                        test.fields.map(entry => {
+                            const label = entry.label;
+                            const name = Test.LabelToName(label);
 
-                    return (
-                        <FormControl key={name} isRequired>
-                            <FormLabel htmlFor={name}>{label}</FormLabel>
-                            <InputGroup>
-                                <Input type='number' name={name} onWheelCapture={e => e.currentTarget.blur()} step='any'/>
-                                <InputRightAddon w='12%'>
-                                    <Center w='100%'>
-                                        { entry.units }
-                                    </Center>
-                                </InputRightAddon>
-                            </InputGroup>
-                        </FormControl>
-                    )
-                })
-            }
+                            return (
+                                <FormControl key={name} isRequired>
+                                    <FormLabel htmlFor={name}>{label}</FormLabel>
+                                    <InputGroup>
+                                        <Input type='number' name={name} onWheelCapture={e => e.currentTarget.blur()} step='any' />
+                                        <InputRightAddon w='12%'>
+                                            <Center w='100%'>
+                                                {entry.units}
+                                            </Center>
+                                        </InputRightAddon>
+                                    </InputGroup>
+                                </FormControl>
+                            )
+                        })
+                    }
 
-                <Button type='submit' size='lg' colorScheme='blue'>Generar PDF</Button>
-            </VStack>
-        </Form>
+                    <FormControl>
+                        <FormLabel htmlFor='observaciones'>Observaciones</FormLabel>
+                        <Textarea name='observaciones' resize={'vertical'} />
+                    </FormControl>
+
+                    <Button type='submit' size='lg' colorScheme='blue'>Generar PDF</Button>
+                </VStack>
+            </Form>
         </Box>
     )
 }
